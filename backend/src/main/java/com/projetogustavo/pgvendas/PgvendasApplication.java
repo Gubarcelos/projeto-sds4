d214d5b5ceb5a7b9ef1388ac29d7b9696276b83f
Gustavo Barcelos <gustavo.barcelos30@gmail.com>
2021-09-07 21:50:14 -0300

    Project created

---
+++ backend/src/main/java/com/projetogustavo/pgvendas/PgvendasApplication.java
@@ -0,0 +1,13 @@
+package com.projetogustavo.pgvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/projetogustavo/pgvendas/PgvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetogustavo.pgvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
